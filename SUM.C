d6ebd857067edbb09956a353258b03d104dc4744
Vaibhav-Bhalala <122871366+Vaibhav-Bhalala@users.noreply.github.com>
2023-01-19 17:43:20 +0530

    Add files via upload

---
+++ SUM.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+      int a,b,c,d,e;
+      float f,g;
+      clrscr();
+      a=20;
+      b=10;
+      c=a+b;
+      d=a-b;
+      e=a*b;
+      f=a/b;
+      g=a%b;
+      printf("the sum of a and b is:%d\n",c);
+      printf("the minus of a and b is:%d\n",d);
+      printf("the multification of a and b is:%d\n",e);
+      printf("the dividation of a and b is:%f\n",f);
+      printf("the percentage of a and b is:%f",g);
+      getch();
+
+
+
+
+
+
+
+}
